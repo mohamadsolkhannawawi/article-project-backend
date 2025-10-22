bb7ba4b87fc7f06a59ff030a55a4091f86f7635d
MohamadSolkhanNawawi <mohamadsolkhan558@gmail.com>
2025-10-22 21:49:55 +0700

    chore: Add debug logging for DATABASE_URL

--- database/database.go
+++ database/database.go
@@ -18,10 +18,13 @@ func ConnectDB() {
 	// Get DSN (Data Source Name) from environment variable
 	dsn := os.Getenv("DATABASE_URL")
 
+	log.Printf("DEBUG: Attempting to connect with DATABASE_URL = [%s]\n", dsn)
+
 	// Open connection to database
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Failed to connect to database:", err)
+		log.Printf("ERROR: Failed to connect to database using DSN [%s]: %v\n", dsn, err)
+		panic(err)
 	}
 
 	log.Println("Successfully connected to database.")
